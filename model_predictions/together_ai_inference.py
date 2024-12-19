fb2ebc9a0634a6253e204f42c3a0d52f7e3a1778
gililior <gili.lior96@gmail.com>
2024-12-19 17:09:19 +0200

    allow neutral prompting

--- model_predictions/together_ai_inference.py
+++ model_predictions/together_ai_inference.py
@@ -8,27 +8,34 @@ from model_predictions.utils import *
 models_list_together = [
     "google/gemma-2-9b-it",
     "google/gemma-2-27b-it",
+    "mistralai/Mistral-7B-Instruct-v0.3"
     "mistralai/Mixtral-8x7B-Instruct-v0.1",
     "mistralai/Mixtral-8x22B-Instruct-v0.1",
     "deepseek-ai/deepseek-llm-67b-chat",
+    "Qwen/Qwen2.5-14B-Instruct"
     "Qwen/Qwen2.5-72B-Instruct"
-    "microsoft/WizardLM-2-8x22B",
     "meta-llama/Llama-3.1-405B-Instruct",
-    "meta-llama/Llama-3.1-70B-Instruct-Turbo",
 ]
 
 
-def run_infer_together(data_df, model_name, out_path):
+def run_infer_together(data_df, model_name, out_path, allow_neutral):
     client = Together()
 
     opposite_framing_pred = []
+    print(allow_neutral)
 
     for i, row in tqdm(data_df.iterrows(), total=len(data_df)):
         opposite_sentiment_framing = get_opposite_framing(row)
-        current_prompt = [
-            {"role": "system", "content": SYSTEM_MSG},
-            {"role": "user", "content": USER_MSG.format(sentence=opposite_sentiment_framing)}
-        ]
+        if allow_neutral:
+            current_prompt = [
+                {"role": "system", "content": SYSTEM_MSG_WITH_NEUTRAL},
+                {"role": "user", "content": USER_MSG_WITH_NEUTRAL.format(sentence=opposite_sentiment_framing)}
+            ]
+        else:
+            current_prompt = [
+                {"role": "system", "content": SYSTEM_MSG},
+                {"role": "user", "content": USER_MSG.format(sentence=opposite_sentiment_framing)}
+            ]
         response = client.chat.completions.create(
             model=model_name,
             messages=current_prompt,
@@ -41,19 +48,21 @@ def run_infer_together(data_df, model_name, out_path):
     return data_df
 
 
-def inference_together(data_path, model_name, out_dir):
+def inference_together(data_path, model_name, out_dir, allow_neutral):
 
     data_df = pd.read_csv(data_path)
 
     os.makedirs(out_dir, exist_ok=True)
     model_name_for_fname = model_name.replace("/", "-")
     out_path = os.path.join(out_dir, f'{model_name_for_fname}_opposite_framing_predictions.csv')
+    if allow_neutral:
+        out_path = out_path.replace(".csv", "_with_neutral.csv")
     print(out_path)
 
     if os.path.exists(out_path):
         data_df = pd.read_csv(out_path)
     else:
-        data_df = run_infer_together(data_df, model_name, out_path)
+        data_df = run_infer_together(data_df, model_name, out_path, allow_neutral)
 
     opposite_framing_pred = data_df['opposite_framing_raw_pred'].tolist()
     processed_out = process_preds(opposite_framing_pred)
@@ -68,9 +77,11 @@ if __name__ == '__main__':
                         help="Path to csv with the data to prompt the model with")
     parser.add_argument("--model_name", type=str, required=True,
                         help="Name of the model to use for inference")
+    parser.add_argument("--allow_neutral", action='store_true', default=False,
+                        help="Name of the model to use for inference")
     parser.add_argument("--out_dir", type=str, default='model_predictions/inference',)
 
     args = parser.parse_args()
-    inference_together(args.data_path, args.model_name, args.out_dir)
+    inference_together(args.data_path, args.model_name, args.out_dir, args.allow_neutral)
 
 
--- model_predictions/utils.py
+++ model_predictions/utils.py
@@ -3,6 +3,9 @@ import re
 SYSTEM_MSG = "You are an advanced language model specializing in sentiment analysis. Your task is to analyze the sentiment of given input text and classify it as either 'Positive,' or 'Negative,' based on the overall tone and emotional content. Do not provide any additional information."
 USER_MSG = "Here's a sentence:\n{sentence}\nIs the sentence Positive or Negative? Write the answer in a Json format: 'sentiment' : 'Positive' or 'Negative'."
 
+SYSTEM_MSG_WITH_NEUTRAL = "You are an advanced language model specializing in sentiment analysis. Your task is to analyze the sentiment of given input text and classify it as either 'Positive,' or 'Negative,' based on the overall tone and emotional content. You can also classify it as 'Neutral', but only if there is no way to determine the dominant sentiment. Do not provide any additional information. "
+USER_MSG_WITH_NEUTRAL = "Here's a sentence:\n{sentence}\nIs the dominant sentiment positive or negative? Write the answer in a Json format: 'sentiment' : 'Positive' or 'Negative' or 'Neutral'."
+
 
 def process_preds(opposite_framing_pred):
     processed_out = []
